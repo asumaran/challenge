9b6202f3e4046f69fe5a48800b568e6e5ca97d90
Alfredo Sumaran <a.sumaran@gmail.com>
2025-12-12 12:10:53 -0500

    Fix duplicated Id

--- packages/api/index.js
+++ packages/api/index.js
@@ -20,7 +20,7 @@ const products = [
   { id: 1, name: "Product 1", reviews: [] },
   { id: 2, name: "Product 2", reviews: [] },
   { id: 3, name: "Product 3", reviews: [] },
-  { id: 3, name: "Product 4", reviews: [] },
+  { id: 4, name: "Product 4", reviews: [] },
 ];
 
 app.get("/products", (req, res) => {
